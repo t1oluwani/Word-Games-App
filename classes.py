d84e20bff3dd117d7b90b024cb89cb4e05a4ef2c
tifav <ti.favour@gmail.com>
2023-01-06 13:20:40 -0700

    first commit

---
+++ classes.py
@@ -0,0 +1,210 @@
+from random import choice
+from collections.abc import MutableSet
+
+# Exception Classes
+class TooShortError(ValueError):
+    pass
+
+class TooLongError(ValueError):
+    pass
+
+class NotAlphaError(ValueError):
+    pass
+
+class Words(MutableSet):
+
+    def __init__(self, letters):
+        '''Initialize empty set of words, letters refer to chosen lenght of words, if letters is 0 then length unspecified'''
+        self.words = set() # initialized as a set to prevent word duplication
+        self.letters = letters
+
+    def __contains__(self, word):
+        ''' Returns True if the word is in the list, returns False otherwise.'''
+        return word.upper() in self.words
+
+    def __iter__(self):
+        ''' Returns an iterator over all the words.'''
+        return iter(self.words)
+
+    def __len__(self):
+        ''' Returns the number of words in the dictionary.'''
+        return len(self.words)
+
+    def add(self, word):
+        '''
+        Adds word to the dictionary. Raises appropriate error if the word is 
+        too short, or too long, or containa anyhing other alphabetical letters.
+        '''        
+        self.check_word(word)
+        self.words.add(word.upper())
+
+    def discard(self, word):
+        ''' Removes word from set.'''
+        self.words.discard(word.upper())
+
+    def check_letters(self, letter):
+        '''HELPER FUNCTION: Raises error if letter isn't in the alphabet'''
+        if not letter.isalpha():
+            raise NotAlphaError ("Word contains letter not in the standard alphabet")
+
+    def check_word(self, word):
+        '''
+        Raises error if word doesn't only contains alphabetic letters 
+        or if word is the incorrect length (> or < self.letters)
+        '''
+        if len(word) < self.letters:
+            raise TooShortError("Word is too short")
+        elif len(word) > self.letters:
+            raise TooLongError("Word is too long")
+        else:
+            for letter in word:
+                self.check_letters(letter)
+
+    def load_filtered(self, data):
+        '''
+        HELPER FUNCTION: adds words to self.words as long as they meet the 
+        specified letter requirements (0 means no specified requirements)
+        '''
+        for line in data:
+            if self.letters == 0:
+                self.words.add(line.strip().upper())
+            elif len(line.strip()) == self.letters:
+                self.words.add(line.strip().upper())
+        
+    def load_file(self, filename):
+        '''Add words from .txt file to set and filter using self.letters'''
+        if filename.endswith(".txt"):
+            with open(filename) as word_file:
+                word_data = word_file.readlines()
+                self.load_filtered(word_data)
+        else:
+            with open(filename+".txt") as word_file:
+                word_data = word_file.readlines()
+                self.load_filtered(word_data)
+
+    def words(self):
+        ''' Returns the number of letters in every word.(Should be same for every word)'''
+        return self.words
+
+    def letters(self):
+        ''' Returns the number of letters in every word.(Should be same for every word)'''
+        return self.letters
+
+    def copy(self):
+        ''' Returns second Words instance which contains the same words.(Shouldn't reload file)'''
+        new_instance = Words(self.letters)
+        for word in self.words:
+            new_instance.add(word)
+        return new_instance
+
+
+class Attempt():
+    def __init__(self, guess, answer):
+        '''
+        Initializes guess with the players guess and the correct answer.
+        '''
+        self.guess = guess
+        self.answer = answer
+
+    def guess(self):
+        ''' Returns the guess that the player made.'''
+        return self.guess
+
+    def answer(self):
+        ''' Returns the correct answer.'''
+        return self.answer
+
+    def win(self):
+        ''' Returns True if players guess is the same as the answer.'''
+        return self.guess == self.answer
+
+    def correct(self): 
+        '''
+        Returns a string that is the same length of the answer. Except consists
+        of underscores everywhere except for where the player guessed correctly.
+        (Green in Wordle) 
+        '''
+        result_str = ''
+        for i in range(len(self.guess)):
+            if self.guess[i] == self.answer[i]:
+                result_str += self.guess[i]
+            else:
+                result_str += "_"
+        return result_str
+
+    def remove_correct(self):
+        '''
+        HELPER FUNCTION: Returns list of letters in guess and list of letters in answer
+        with all the letters that were correctly placed removed
+        '''
+        guess_list, answer_list = [], []
+        guess_list[:0] = self.guess # initilized guessed word as a list
+        answer_list[:0] = self.answer # initialize answer word as a list
+        del_list = [] # initilize list of to be deleted letters
+
+        # add all the letters guessed correctly to a list
+        for guess_str, answer_str in zip(guess_list, answer_list): 
+            if guess_str == answer_str:
+                del_list.append(guess_str)
+        # remove letters guessed correctly from guess list and answer list
+        for str in del_list: 
+            guess_list.remove(str)
+            answer_list.remove(str)
+        return guess_list, answer_list
+        
+
+    def misplaced(self):
+        '''
+        Returns a ------ string that contains every letter which the player 
+        guessed that is also in the answer, but not at the same position.
+        (Yellow in Wordle)
+        '''
+        misplaced_str = ''
+        guess_list, answer_list = self.remove_correct()
+        # add all the letters that are in the answer but in the wrong position to a list
+        for letter in guess_list: 
+            if letter in answer_list:
+                misplaced_str += letter
+        # return ''.join(sorted(misplaced_str))
+        return ''.join(misplaced_str)
+
+
+    def incorrect(self):
+        '''
+        Returns a ------ string that contains every letter which the player 
+        guessed that was not in the answer at all. 
+        (Grey in Wordle)
+        '''
+        incorrect_str = ''
+        guess_list, answer_list = self.remove_correct()
+        # add all the letters that aren't in the answer to a list
+        for letter in guess_list: 
+            if not letter in answer_list:
+                incorrect_str += letter
+        # return ''.join(sorted(incorrect_str))
+        return ''.join(incorrect_str)
+
+
+class Game():
+    def __init__(self, words):
+        ''' Take a Words instance and picks a random word for the game.'''
+        self.attempts = 0
+        self.random_word = choice(tuple(words.words))
+
+    def num_attempts(self):
+        ''' Return the number of attempts the player has made so far.'''
+        return self.attempts
+    
+    def answer(self):
+        ''' Returns the random word chosen to be the answer'''
+        return self.random_word
+
+    def attempt(self, players_guess):
+        ''' 
+        Returns an Attempt instance object that represents the 
+        results of whatever guess the player makes.
+        '''
+        self.attempts += 1
+        return Attempt(players_guess, self.random_word)
+    
+
---
+++ main.py
@@ -0,0 +1,232 @@
+from classes import Game
+from classes import Words
+from classes import Attempt
+from classes import TooLongError
+from classes import TooShortError
+from classes import NotAlphaError
+
+class WordGame:
+    '''
+    This class initializes the components that a word game would need to work
+    such as selected a word to use, comparing it with the players guess and etc
+    '''
+    def __init__(self):
+        self.misplaced = ''
+        self.displaced = ''
+        self.incorrect = ''
+        
+    def get_letters(self):
+        ''' Returns amount of letters player would like to play with'''
+        while True:
+            entry = input("How many letters would you like to play with (0 to unspecify):\n")
+            try:
+                return int(entry)
+            except:
+                print("Please enter a valid whole number!")
+                
+    def get_guess(self):
+        ''' Returns players guess'''
+        while True:
+            entry = input("Enter a "+str(self.letters)+" letter word:\n")
+            try:
+                assert len(entry) == self.letters
+                return str(entry)
+            except:
+                print("Please enter a "+str(self.letters)+" letter string!")
+                
+    def merge_dash(self, dash_str1, dash_str2):
+        ''' TOWRITE'''
+        result_str = ''
+        for i in range(len(dash_str1)):
+            if dash_str1[i] != "_":
+                result_str += dash_str1[i]
+            elif dash_str2[i] != "_":
+                result_str += dash_str2[i]
+            else:
+                result_str += "_"
+        return result_str
+        
+        
+    def victory(self):
+        attempts = self.game.num_attempts()
+        if attempts == 1:
+            print("Congrats, you have won in "+str(attempts)+" try and with a perfect your score of ----")
+        else :
+            print("Congrats, you have won in "+str(attempts)+" tries, your score is ----")
+ 
+
+            
+    def make_attempt(self):
+        ''' Make an attempt by guess a word of the required size'''
+        print("")
+        print(self.game.answer())
+        
+        self.guess = self.get_guess()
+        self.guess = self.guess.upper()
+        self.attempt = self.game.attempt(self.guess)
+        
+        self.correct = self.merge_dash(self.correct, self.attempt.correct())
+        self.displaced += self.attempt.misplaced()
+        self.displaced = "".join(sorted(set(self.displaced)))
+        self.incorrect += self.attempt.incorrect()
+        self.incorrect = "".join(sorted(set(self.incorrect)))
+        
+        if self.attempt.win():
+            self.victory()
+        else:
+            self.re_attempt()
+        
+        
+    def re_attempt(self):
+        print("Your Guess       :", self.guess)
+        print("Correct Letters  :", self.correct)
+        print("Misplaced Letters:", self.attempt.misplaced())
+        print("Displaced Letters:", self.displaced)
+        print("Incorrect Letters:", self.incorrect)
+        self.make_attempt()
+        
+        
+    def play_again(self):
+        again = True
+        while again == True:
+            entry = input("Would you like to play again Yes(Y) or No(N):\n").lower()
+            if entry[0] == "y":
+                again = False
+                self.new_game()
+            elif entry[0] == "n":
+                again = False
+                print("Thank you for playing!")
+                start_game()
+            else:
+                print("Please enter a valid option!")
+
+    def new_game(self):
+        '''
+        Initialize new game
+        '''
+        # Gets self.letters from player
+        self.letters = self.get_letters()
+        
+        # Load words to self.words
+        self.words = Words(self.letters)
+        self.words.load_file("words.txt") #replace w library
+        
+        # Initialize game using self.words
+        self.game = Game(self.words)
+        self.letters = len(self.game.answer()) # reassign value to letter to length of answer word
+        self.score = 1000*  
+        self.correct = "_"*self.letters
+        
+        # Make an attempt
+        self.make_attempt()
+        
+        # Asks user if they want to play again with a different word
+        self.play_again()
+        
+        print("newgame")
+
+    
+
+
+class Hangman(WordGame):
+    '''
+    This class extends WordGame and uses the Game, Words, and Attempt classes
+    to create a Hangman user interface experience on the console
+    '''
+    ...
+
+class Wordle(WordGame):
+    '''
+    This class extends WordGame and uses the Game, Words, and Attempt classes
+    to create a Wordle user interface experience on the console
+    '''
+    ...
+
+def learn_rules():
+    '''
+    Displays rules and synopsis about users selected game
+    '''
+    choosing = True
+    while choosing == True:
+        entry = input("What would you like to learn about, Hangman(H), Wordle(W), Esc(E):\n").lower()
+
+        if entry[0] == "h":
+            choosing = False
+            print("""
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Proin egestas, sapien vel ornare vestibulum, felis urna 
+                  volutpat tellus, in rutrum ex massa eu lorem. Cras at 
+                  aliquam sem. Lorem ipsum dolor sit amet, consectetur 
+                  adipiscing elit. Proin vel lorem at orci elementum.
+                  """)
+            start_game()
+            
+        elif entry[0] == "w":
+            choosing = False
+            print("""
+                  Nam ornare massa non turpis placerat faucibus. Fusce luctus, 
+                  justo a imperdiet lacinia, risus ante scelerisque ex, sodales 
+                  eleifend risus mi non leo. Nunc id consectetur augue. Morbi 
+                  euismod volutpat neque, vel ullamcorper mauris. Proin ut venenatis 
+                  massa. Donec urna augue, laoreet vitae lobortis ut, sagittis in. 
+                  """)
+            start_game()
+            
+        elif entry[0] == "e":
+            choosing = False
+            start_game()
+        else:
+            print("Please enter a valid option!")
+            
+def choose_game():
+    '''
+    Initializes the word games experience by asking player which game they want to play
+    '''
+    choosing = True
+    while choosing == True:
+        entry = input("What would you like to play, Hangman(H), Wordle(W), Esc(E):\n").lower()
+
+        if entry[0] == "h":
+            choosing = False
+            Hangman().new_game()
+        elif entry[0] == "w":
+            choosing = False
+            Wordle().new_game()
+        elif entry[0] == "e":
+            choosing = False
+            start_game()
+        else:
+            print("Please enter a valid option!")
+
+
+def start_game():
+    '''
+    Initializes the word games experience by asking player if they want to play, see the rules or leave
+    '''
+    start = True
+    while start == True:
+        entry = input("""
+    Hello and Welcome to Word Games!
+    If you know the rule and would like to play, enter Play(P)
+    If you do no tknow how to play and would like to learn, enter Help(H)
+    If you would like to exit this experience, enter Esc(E)
+    -> """).lower()
+
+        if entry[0] == "p":
+            start = False
+            print("")
+            choose_game()
+        elif entry[0] == "h":
+            print("")
+            start = False
+            learn_rules()
+        elif entry[0] == "e":
+            start = False
+        else:
+            print("Please enter a valid option!")
+
+def main():
+    start_game()
+    
+if __name__=="__main__":
+    main()
